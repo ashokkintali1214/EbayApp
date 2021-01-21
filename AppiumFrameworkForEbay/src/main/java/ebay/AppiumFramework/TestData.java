c732a96d130f33b18053316c679a8e2889896c3c
Ashok kumar <kintali.ashok@gmail.com>
2021-01-21 14:02:14 +0530

    added logers

--- AppiumFrameworkForEbay/src/main/java/ebay/AppiumFramework/TestData.java
+++ AppiumFrameworkForEbay/src/main/java/ebay/AppiumFramework/TestData.java
@@ -2,7 +2,7 @@ package ebay.AppiumFramework;
 
 
 public class TestData {		
-		public String mobileNumber = "6281185775";
-		public String password = "Vivo@2020";
+		public String mobileNumber = "aaaa";
+		public String password = "xxx";
 		public String searchText = "65-inch-Tv";
 	}
--- AppiumFrameworkForEbay/src/test/java/ebay/AppiumFramework/AmazonAppTest.java
+++ AppiumFrameworkForEbay/src/test/java/ebay/AppiumFramework/AmazonAppTest.java
@@ -28,7 +28,7 @@ public class AmazonAppTest extends base {
 	public void testAmazonAPK() throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		 service=startServer();
-		Log.info("started");
+		Log.info("******************started*************");
 		AndroidDriver<AndroidElement> driver = capabilities("apiDemo");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
@@ -48,13 +48,11 @@ public class AmazonAppTest extends base {
 		String devicePrice = searchResultsPage.getDevicePriceOnSearchResultsPage(2);
 		searchResultsPage.clickProductInSearchResultsPage(2);
 
-		System.out.print(deviceName + " and " + devicePrice);
-
 		ProductDetailPage pdp = new ProductDetailPage(driver);
 		pdp.verifyPDPLoaded(driver);
 		String deviceNameInPDP = pdp.getDeviceNameOnPDPPage();
 		String devicePriceInPDP = pdp.getDevicePriceOnPDPPage();
-		System.out.print(deviceNameInPDP + " and " + devicePriceInPDP);
+		
 		pdp.clickAddToCartCTA(driver);
 
 		CartPage cartPage = new CartPage(driver);
@@ -66,18 +64,24 @@ public class AmazonAppTest extends base {
 		Assert.assertEquals(deviceName, deviceNameInPDP,
 				"Device names are not same in Search results page and Product detail page");
 		Reporter.log("Device names are same in Search results page and Product detail page");
+		Log.info("Device names are same in Search results page and Product detail page");
 		Assert.assertEquals(devicePrice, devicePriceInPDP,
 				"Device Prices are not same in Search results page and Product detail page");
 		Reporter.log("Device Prices are same in Search results page and Product detail page");
+		Log.info("Device Prices are same in Search results page and Product detail page");
 		Assert.assertEquals(deviceNameInPDP, deviceNameInCart,
 				"Device names are not same in Product detail page and Cart Page");
 		Reporter.log("Device names are same in Product detail page and Cart Page");
+		Log.info("Device names are same in Product detail page and Cart Page");
 		Assert.assertEquals(devicePriceInPDP, devicePriceInCart,
 				"Device Prices are not same in Product detail page and Cart Page");
 		Reporter.log("Device Prices are same in Product detail page and Cart Page");
+		Log.info("Device Prices are same in Product detail page and Cart Page");
+		
+		Log.info("******************stopped*************");
 
 		driver.closeApp();
-		service.stop();
+	//	service.stop();
 	}
 
 }
